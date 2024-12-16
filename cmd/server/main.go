c6d425832119a390387b43c4bd0ae807c59e2cbe
Mark013131 <167769265+Mark013131@users.noreply.github.com>
2024-12-16 13:26:57 +0900

    serverの調整

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	// サーバーのインスタンスを作成
 	s := server.NewServer()
 
-	// サーバーを起動して、エラーが発生した場合にはログに出力
 	log.Println("Starting the server on :8080...")
 	if err := s.Start(); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
